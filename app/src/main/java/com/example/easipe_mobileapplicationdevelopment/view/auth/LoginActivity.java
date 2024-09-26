6ba4eb187f7c561bc3e4f13a96f2a056134a1f73
MishelLiyanage <mailtomishel@gmail.com>
2024-09-26 23:46:13 +0530

    Login Backend Done

--- app/src/main/java/com/example/easipe_mobileapplicationdevelopment/view/auth/LoginActivity.java
+++ app/src/main/java/com/example/easipe_mobileapplicationdevelopment/view/auth/LoginActivity.java
@@ -1,34 +1,85 @@
-//Started - 2024-09-15  Author - Mishel Fernando StudentID - IM/2021/115
+//Created - 2024-09-15  Author - Mishel Fernando StudentID - IM/2021/115
+//Backend updated - 2024-09-26  Author - Mishel Fernando StudentID - IM/2021/115
 
 package com.example.easipe_mobileapplicationdevelopment.view.auth;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.easipe_mobileapplicationdevelopment.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText editTextEmail, editTextPassword;
+    private FirebaseAuth authProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        editTextEmail = findViewById(R.id.editTextTextEmail);
+        editTextPassword = findViewById(R.id.editTextTextPassword);
+
+        //check whether the user has already logged in
+        authProfile = FirebaseAuth.getInstance();
+
+        //Login User
+        Button loginBtn = findViewById(R.id.SigninBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(LoginActivity.this, "Please enter your email", Toast.LENGTH_SHORT).show();
+                    editTextEmail.setError("Email is required");
+                    editTextEmail.requestFocus();
+                } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    Toast.makeText(LoginActivity.this, "Please re-enter your email", Toast.LENGTH_SHORT).show();
+                    editTextEmail.setError("Valid email is required");
+                    editTextEmail.requestFocus();
+                } else if (TextUtils.isEmpty(password)){
+                    Toast.makeText(LoginActivity.this, "Please enter your password", Toast.LENGTH_SHORT).show();
+                    editTextPassword.setError("Password is required");
+                    editTextPassword.requestFocus();
+                } else {
+                    loginUser(email, password);
+                }
+            }
+        });
+
+    }
+
+    private void loginUser(String email, String password) {
+        authProfile.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this, "User has logged in now", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 
-//    redirect to registration page
+    //    redirect to registration page
     public void redirectToRegister(View view) {
         Intent intent = new Intent(this, RegistrationActivity.class);
         startActivity(intent); // Redirect to NextActivity
@@ -41,4 +92,4 @@ public class LoginActivity extends AppCompatActivity {
     }
 }
 
-//Finished - 2024-09-15  Author - Mishel Fernando StudentID - IM/2021/115
+//Finished - 2024-09-26  Author - Mishel Fernando StudentID - IM/2021/115
