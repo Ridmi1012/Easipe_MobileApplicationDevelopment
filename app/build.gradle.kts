ad7011ee11902f69dc3eed49ce1ad38d46d17e5c
Hirun Senarathna <hirunsenarathna@users.noreply.github.com>
2024-10-04 10:57:58 +0530

    do some changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,9 +37,10 @@ android {
 
 dependencies {
 
-    implementation("com.github.bumptech.glide:glide:4.16.0")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
-    implementation ("com.firebaseui:firebase-ui-database:8.0.0")
+    implementation(libs.glide)
+    annotationProcessor(libs.compiler)
+    implementation (libs.firebase.ui.database)
+    implementation(libs.play.services.auth)
 
     implementation(libs.appcompat)
     implementation(libs.material)
